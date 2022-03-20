1f97609653f413c229850430acc63db16170879f
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-20 13:03:03 +0530

    Added: Calculate Fare logic

--- src/main/java/com/bridgelabz/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoice.java
@@ -1,6 +1,17 @@
 package com.bridgelabz;
 
 public class CabInvoice {
+	private static final double MINIMUM_COST_PER_KM = 10.0;
+	private static final int COST_PER_TIME = 1;
+	private static final int MINIMUM_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		if (totalFare < MINIMUM_FARE)
+			return MINIMUM_FARE;
+		else
+			return totalFare;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Cab Invoice Generator...");
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,5 +1,15 @@
 package com.bridgelabz;
 
-public class CabInvoiceTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class CabInvoiceTest {
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		CabInvoice invoiceGenerator = new CabInvoice();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
 }
